993af0511b9ca9c80141c8cf76205e1ebd67a21a
jarodmellus <jmellus6899@gmail.com>
2022-07-20 14:02:37 -0400

    fixed expanded/collapsed behavior

--- main.js
+++ main.js
@@ -19,20 +19,19 @@ function openTab(page) {
     e.classList.toggle('expanded')
     selected=page;
 }
-/*
+
 $(function(){
-    var $jittery = $('.jittery'),
-        aText    = $jittery.text().split(''),
-        letters = '';
-    
-    for(var i = 0; i < aText.length; i++){
-      letters += '<span>'+aText[i]+'</span>';
-    }
-    
-    $jittery.empty().append(letters);
-    
-    $.each($('span', $jittery), function(i){
-      $(this).css('animation-delay', '-'+i+'70ms');
-    });
+  var $jittery = $('.jittery'),
+      aText    = $jittery.text().split(''),
+      letters = '';
+  
+  for(var i = 0; i < aText.length; i++){
+    letters += '<span>'+aText[i]+'</span>';
+  }
+  
+  $jittery.empty().append(letters);
+  
+  $.each($('span', $jittery), function(i){
+    $(this).css('animation-delay', '-'+i+'70ms');
   });
-  */
+});
